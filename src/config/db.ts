75edc90d87964d99dbfa30c1225f055c1b531e8b
FrancyAmada <francyamada1983@gmail.com>
2026-04-15 03:48:56 +0800

    feat: migrated enav auth to supabase

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,6 +4,12 @@ import { createClient } from '@supabase/supabase-js';
 require('dotenv').config();
 
 export const supabase = createClient(
-  process.env.EXPO_PUBLIC_SUPABASE_URL! as string,
-  process.env.EXPO_PUBLIC_SUPABASE_SERVICE_ROLE_KEY! as string,
+  process.env.SUPABASE_URL! as string,
+  process.env.SUPABASE_SERVICE_ROLE_KEY! as string,
+  {
+    auth: {
+      persistSession: false,
+      autoRefreshToken: false,
+    },
+  },
 );
