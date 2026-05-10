b36739d961f3c17c99062c9ab48942812f82efba
FrancyAmada <francyamada1983@gmail.com>
2026-05-11 04:31:59 +0800

    feat: detach user token on logout

--- src/controllers/push-tokens/attachPushTokenToUser.ts
+++ src/controllers/push-tokens/attachPushTokenToUser.ts
@@ -3,29 +3,37 @@ import { Request, Response } from 'express';
 
 export const attachPushTokenToUser = async (req: Request, res: Response) => {
   const user = req.user;
-  const { token } = req.body;
+  const { token, deviceId, platform } = req.body;
 
-  console.log('--- ATTACH TOKEN DEBUG ---');
-  console.log('Authorization header:', req.headers.authorization);
-  console.log('User:', req.user?.userId);
-  if (!token || !user?.userId) {
+  if (!token || !user?.userId || !deviceId) {
     return res.status(400).json({ error: 'Missing data' });
   }
 
-  const { error } = await supabase.from('UserPushTokens').upsert(
-    {
-      token,
-      user_id: user.userId,
-      updated_at: new Date().toISOString(),
-    },
-    {
-      onConflict: 'token',
-    },
-  );
+  const now = new Date().toISOString();
 
-  if (error) {
-    return res.status(500).json({ error: error.message });
+  try {
+    const { error } = await supabase.from('UserPushTokens').upsert(
+      {
+        device_id: deviceId,
+        token,
+        user_id: user.userId,
+        platform,
+        updated_at: now,
+        last_seen_at: now,
+      },
+      {
+        onConflict: 'device_id',
+      },
+    );
+
+    if (error) {
+      console.error(error);
+      return res.status(500).json({ error: error.message });
+    }
+
+    return res.status(200).json({ success: true });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: 'Internal server error' });
   }
-
-  return res.status(200).json({ success: true });
 };
--- src/routes/notifications.routes.ts
+++ src/routes/notifications.routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import { attachPushTokenToUser } from '../controllers/push-tokens/attachPushTokenToUser';
 import { registerPushToken } from '../controllers/push-tokens/registerPushToken.controller';
 import { authenticatePatient } from '../middleware/auth';
+import { detachPushTokenFromUser } from '../controllers/push-tokens/detachPushTokenFromUser';
 
 const router = Router();
 
 router.post('/register-device-token', registerPushToken);
 router.post('/attach-token', authenticatePatient, attachPushTokenToUser);
+router.post('/detach-token', authenticatePatient, detachPushTokenFromUser);
 
 export default router;
